69094e7e8f4a00003d821627ada7359e1a55f9c8
xielei <xl819202558@qq.com>
2019-07-03 20:21:49 +0800

    添加dubbo依赖,并支持RPC调用

--- bfxy-order/src/main/java/com/bfxy/bfxyorder/entity/Order.java
+++ bfxy-order/src/main/java/com/bfxy/bfxyorder/entity/Order.java
@@ -1,10 +1,88 @@
 package com.bfxy.bfxyorder.entity;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * 描述：
  *
  * @author xielei
  * @date 2019/07/01
  */
-public class Order {
+@Data
+@Table(name = "tb_order")
+@ToString
+public class Order implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * order_id
+     */
+    private String orderId;
+    
+    /**
+     * order_type
+     */
+    private String orderType;
+    
+    /**
+     * city_id
+     */
+    private String cityId;
+    
+    /**
+     * platform_id
+     */
+    private String platformId;
+    
+    /**
+     * user_id
+     */
+    private String userId;
+    
+    /**
+     * supplier_id
+     */
+    private String supplierId;
+    
+    /**
+     * goods_id
+     */
+    private String goodsId;
+    
+    /**
+     * order_status
+     */
+    private String orderStatus;
+    
+    /**
+     * remark
+     */
+    private String remark;
+    
+    /**
+     * create_by
+     */
+    private String createBy;
+    
+    /**
+     * create_time
+     */
+    private Date createTime;
+    
+    /**
+     * update_by
+     */
+    private String updateBy;
+    
+    /**
+     * update_time
+     */
+    private Date updateTime;
 }
